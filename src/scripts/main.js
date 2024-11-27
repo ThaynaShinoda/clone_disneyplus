86022343fb569d96f0cedecfe838a6e22aaa655a
Thayná Rie Shinoda <thayrieshinoda@gmail.com>
2024-11-27 15:40:37 -0300

    Criando seção de FAQ

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Selecionou TODOS os botões com o atributo DATA-TAB-BUTTON
     const buttons = document.querySelectorAll('[data-tab-button]')
     
+    const questions = document.querySelectorAll('[data-faq-question]')
 
     for(let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao){
@@ -16,7 +17,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
         })
     }
+
+    for(let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', openCloseAnswer)
+    }
 })
+
+function openCloseAnswer(elemento) {
+    const classe = 'faq__list__item--is-open'
+    const elementoPai = elemento.target.parentNode
+
+    elementoPai.classList.toggle(classe)
+}
 
 function removeActiveBtn() {
     const buttons = document.querySelectorAll('[data-tab-button]')

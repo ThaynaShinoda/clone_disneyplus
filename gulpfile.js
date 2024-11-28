971cc90a763428bb649ee25e0e3418b4ffa2c694
Thayná Rie Shinoda <thayrieshinoda@gmail.com>
2024-11-28 14:46:24 -0300

    Configurando uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require ('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
@@ -14,7 +15,15 @@ function comprimeImg() {
         .pipe(gulp.dest('./dist/images'))
 }
 
-exports.default = gulp.parallel(styles, comprimeImg);
+function comprimeScript() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/scripts'))
+}
+
+exports.default = gulp.parallel(styles, comprimeImg, comprimeScript);
+
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss',{ignoreInitial:false}, gulp.parallel(styles));
+    gulp.watch('./src/scripts/*.js', gulp.parallel(comprimeScript));
 }
